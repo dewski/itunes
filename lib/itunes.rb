9c1f3f0ed9eb97b5c6b6eddfddd2d5d61a681bbc
dewski <xhtmlthis@me.com>
2010-10-02 20:16:04 -0700

    added helper methods for all types of queries

--- lib/itunes.rb
+++ lib/itunes.rb
@@ -18,6 +18,39 @@ class ITunes
       super(name, *args)
     end
   end
+  
+  # So you don't have to create an instance if you don't need to
+  def self.music(terms)
+    self.new.music(terms)
+  end
+  
+  def self.podcast(terms)
+    self.new.podcast(terms)
+  end
+  
+  def self.movie(terms)
+    self.new.movie(terms)
+  end
+  
+  def self.music_video(terms)
+    self.new.music_video(terms)
+  end
+  
+  def self.audiobook(terms)
+    self.new.audiobook(terms)
+  end
+  
+  def self.short_film(terms)
+    self.new.short_film(terms)
+  end
+  
+  def self.tv_show(terms)
+    self.new.tv_show(terms)
+  end
+  
+  def self.all(terms)
+    self.new.all(terms)
+  end
 
   private
     def request(term, media='all')
--- lib/itunes/version.rb
+++ lib/itunes/version.rb
@@ -1,3 +1,3 @@
 class ITunes
-  VERSION = '0.1.0'.freeze
+  VERSION = '0.2.0'.freeze
 end
