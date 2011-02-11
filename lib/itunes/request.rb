3b730aa596249c4d80efdb91e397b3703973dfd6
Steve Agalloco <steve.agalloco@gmail.com>
2011-02-11 00:01:08 -0500

    made request and connection methods private

--- lib/itunes/request.rb
+++ lib/itunes/request.rb
@@ -1,28 +1,30 @@
 module ITunes
   module Request
+    # @private
+    private
 
-    # Perform an HTTP GET request
-    def request(request_type, params)
-      url = '/WebObjects/MZStoreServices.woa/wa/ws' + request_type
+      # Perform an HTTP GET request
+      def request(request_type, params)
+        url = '/WebObjects/MZStoreServices.woa/wa/ws' + request_type
 
-      response = connection.get do |req|
-        req.url url, params
+        response = connection.get do |req|
+          req.url url, params
+        end
+        response.body
       end
-      response.body
-    end
 
-    def connection
-      options = {
-        :headers => {'Accept' => "application/json", 'User-Agent' => user_agent},
-        :url => api_endpoint,
-      }
+      def connection
+        options = {
+          :headers => {'Accept' => "application/json", 'User-Agent' => user_agent},
+          :url => api_endpoint,
+        }
 
-      Faraday::Connection.new(options) do |builder|
-        builder.adapter(adapter)
-        builder.use Faraday::Response::ParseJson
-        builder.use Faraday::Response::Rashify
+        Faraday::Connection.new(options) do |builder|
+          builder.adapter(adapter)
+          builder.use Faraday::Response::ParseJson
+          builder.use Faraday::Response::Rashify
+        end
       end
-    end
 
   end
 end
