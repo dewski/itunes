2c4e1621e6a26276893d6463db634da0f1bb095d
dewski <me@garrettbjerkhoel.com>
2012-04-18 16:05:39 -0700

    Move methods to same line as private

--- lib/itunes/request.rb
+++ lib/itunes/request.rb
@@ -4,29 +4,28 @@ module ITunes
     # @private
     private
 
-      # Perform an HTTP GET request
-      def request(request_type, params)
-        url = '/WebObjects/MZStoreServices.woa/wa/ws' + request_type
+    # Perform an HTTP GET request
+    def request(request_type, params)
+      url = '/WebObjects/MZStoreServices.woa/wa/ws' + request_type
 
-        response = connection.get do |req|
-          req.url url, params
-          req.options = request_options
-        end
-        response.body
+      response = connection.get do |req|
+        req.url url, params
+        req.options = request_options
       end
+      response.body
+    end
 
-      def connection
-        options = {
-          :headers => {'Accept' => 'application/json', 'User-Agent' => user_agent},
-          :url => api_endpoint,
-        }
+    def connection
+      options = {
+        :headers => {'Accept' => 'application/json', 'User-Agent' => user_agent},
+        :url => api_endpoint,
+      }
 
-        Faraday.new(options) do |builder|
-          builder.use Faraday::Response::Rashify
-          builder.use Faraday::Response::ParseJson
-          builder.adapter(adapter)
-        end
+      Faraday.new(options) do |builder|
+        builder.use Faraday::Response::Rashify
+        builder.use Faraday::Response::ParseJson
+        builder.adapter(adapter)
       end
-
+    end
   end
 end
